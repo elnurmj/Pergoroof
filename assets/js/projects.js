5ec12f52c572064ad30aba94419ab7af894d0c08
Elnur <elnurmj@code.edu.az>
2022-12-21 10:07:36 +0300

    Sidebar update

---
+++ assets/js/projects.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "264px";
+    document.getElementById("main").style.marginLeft = "0px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
