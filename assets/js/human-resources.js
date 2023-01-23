9bb83599da0c3fd2d7518371936b7c56a445094d
Elnur <elnurmj@code.edu.az>
2023-01-23 16:34:15 +0300

    updated

---
+++ assets/js/human-resources.js
@@ -0,0 +1,14 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "264px";
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("main").style.marginLeft = "264px";
+    document.getElementById("navbar").style.marginLeft = "264px";
+    document.getElementById("openbtn").style.display = "none";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("navbar").style.marginLeft = "0";
+    document.getElementById("openbtn").style.display = "block";
+  }
