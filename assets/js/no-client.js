b99a3dfe22016f58805578929046799b662c65a5
Elnur <elnurmj@code.edu.az>
2023-02-15 14:59:19 +0300

    update

--- assets/js/no-client.js
+++ assets/js/no-client.js
@@ -12,7 +12,3 @@ function openNav() {
     document.getElementById("navbar").style.marginLeft = "0";
     document.getElementById("openbtn").style.display = "block";
   }
-
-  function closeFilter() {
-    document.getElementById("filter").style.display = "none";
-  }
